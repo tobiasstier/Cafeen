607d7e90e1e79b82c2b272e9ea16ecd36ff7267c
Jonathan-Greve <jonagrev@gmail.com>
2016-06-13 15:57:02 +0200

    Revert "sdf"
    This reverts commit 32faf9ef9e59894e83b588b54f494dc9830b7852.

--- Cafeen.Tests/UnitTest1.cs
+++ Cafeen.Tests/UnitTest1.cs
@@ -1,15 +1,65 @@
-﻿using System;
+﻿using Cafeen.Controllers;
+using Cafeen.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
 
-namespace Cafeen.Tests
+namespace Cafeentest.Controllers
 {
     [TestClass]
-    public class UnitTest1
+    public class HomeControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestDefaultView()
         {
-           
+            var controller = new HomeController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("", result.ViewName);
         }
     }
+    [TestClass]
+    public class AccountingsControllerTest
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            var controller = new AccountingsController();
+            var result = controller.Create() as ViewResult;
+            Assert.AreEqual("", result.ViewName);
+        }
+
+
+        //[TestMethod]
+        //public void TestLock()
+        //{
+        //    var controller = new AccountingsController();
+        //    var Accounting = new Accounting();
+        //    var result = controller.Lock(1) as 
+        //}
+
+
+
+        //[TestMethod]
+        //public void testEditAccounting()
+        //{
+        //    var controller = new AccountingsController();
+        //    var accounting = new Accounting();
+        //    var Id = 4;
+        //    var StartCash = 1000;
+        //    var EndCash = 500;
+        //    var CardCash = 2500;
+        //    var TimeStamp = System.DateTime.Now;
+        //    var LockStatus = false;
+        //    var StartProduct = "bob";
+        //    var EndProduct = "bobelina";
+        //    var result = (RedirectToRouteResult) controller.Edit(accounting);
+        //    Assert.AreEqual("Index", result.Values["action"]);
+        //}
+    }
 }
+
+
+
+
+
+
+
